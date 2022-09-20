e69f408e421eaee7f3f23ed3e6c09b227a4d737c
Hou Wai Wan (Hugh) <wanhouwai@gmail.com>
2022-09-20 14:01:11 -0700

    stuff

--- twitch_bot.py
+++ twitch_bot.py
@@ -114,10 +114,8 @@ class waikinBot(commands.Bot):
     @commands.command(name="commands")
     async def waikinBotDesc(self, ctx: commands.Context) -> None:
         
-        print(type(ctx.author._display_name))
-        print(ctx.author._display_name)
         await ctx.send(f"""
-        @{ctx.author.display_name}List of commands: !hello (Bot says hello to you) || !waikinbot (Overview of bot and link to github) || !start (Start tracking session statistics) ||
+        @{ctx.author.display_name} List of commands: !hello (Bot says hello to you) || !waikinbot (Overview of bot and link to github) || !start (Start tracking session statistics) ||
         !stop (Stop tracking session statistics) || !bestgame (Returns a message with the best game in the session)
         """)
 
